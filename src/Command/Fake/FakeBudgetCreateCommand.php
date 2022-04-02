1d5052b0652cf12c982d38297b93d2bf9e9be2b3
Starozytny <starozytny@hotmail.fr>
2022-04-02 11:50:34 +0200

    fake data budget

---
+++ src/Command/Fake/FakeBudgetCreateCommand.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Command\Fake;
+
+use App\Entity\Budget\BuItem;
+use App\Entity\User;
+use App\Service\Data\Budget\DataItem;
+use App\Service\DatabaseService;
+use Doctrine\ORM\EntityManagerInterface;
+use Faker\Factory;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class FakeBudgetCreateCommand extends Command
+{
+    protected static $defaultName = 'fake:budget:create';
+    protected static $defaultDescription = 'Create fake budget';
+    private $em;
+    private $databaseService;
+    private $dataEntity;
+
+    public function __construct(EntityManagerInterface $entityManager, DatabaseService $databaseService, DataItem $dataEntity)
+    {
+        parent::__construct();
+
+        $this->em = $entityManager;
+        $this->databaseService = $databaseService;
+        $this->dataEntity = $dataEntity;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $io->title('Reset des tables');
+        $this->databaseService->resetTable($io, [BuItem::class]);
+
+        $user = $this->em->getRepository(User::class)->findOneBy(['username' => "shanbo"]);
+        $now = new \DateTime();
+
+        $io->title('Création de 500 budgets fake');
+        $fake = Factory::create();
+        for($i=0; $i<500 ; $i++) {
+
+            $data = [
+                "year" => $now->format("Y"),
+                "month" => $fake->numberBetween(1, 12),
+                "type" => $fake->numberBetween(0, 2),
+                "price" => $fake->randomFloat(1),
+                "name" => $fake->name
+            ];
+
+            $data = json_decode(json_encode($data));
+
+            $new = $this->dataEntity->setData(new BuItem(), $data);
+            $new->setUser($user);
+
+            $this->em->persist($new);
+        }
+        $io->text('BUDGET : Budget fake créés' );
+
+        $this->em->flush();
+
+        $io->newLine();
+        $io->comment('--- [FIN DE LA COMMANDE] ---');
+        return Command::SUCCESS;
+    }
+}
---
+++ src/Service/Data/Budget/DataItem.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace App\Service\Data\Budget;
+
+
+use App\Entity\Budget\BuItem;
+use App\Entity\User;
+use App\Service\SanitizeData;
+
+class DataItem
+{
+    private $sanitizeData;
+
+    public function __construct(SanitizeData $sanitizeData)
+    {
+        $this->sanitizeData = $sanitizeData;
+    }
+
+    public function setData(BuItem $obj, $data): BuItem
+    {
+        return ($obj)
+            ->setYear($this->sanitizeData->setToInteger($data->year, 0))
+            ->setMonth($this->sanitizeData->setToInteger($data->month, 0))
+            ->setType($this->sanitizeData->setToInteger($data->type, 0))
+            ->setPrice($this->sanitizeData->setToFloat($data->price, 0))
+            ->setName($this->sanitizeData->trimData($data->name))
+        ;
+    }
+}
