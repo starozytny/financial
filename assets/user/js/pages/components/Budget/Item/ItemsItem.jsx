8b2f56a2e88d605febb4d581bd3c28f75ef296f2
Starozytny <starozytny@hotmail.fr>
2022-04-09 13:23:05 +0200

    btn to prevision to active

--- assets/user/js/pages/components/Budget/Item/ItemsItem.jsx
+++ assets/user/js/pages/components/Budget/Item/ItemsItem.jsx
@@ -28,7 +28,13 @@ export class ItemsItem extends Component {
                 <div className="item-body">
                     <div className="infos infos-col-4">
                         <div className="col-1">
-                            <div className="sub">{elem.isActive ? elem.updatedAtString ? elem.updatedAtString : elem.createdAtString : <span className="badge badge-5">Prévisionnel</span>}</div>
+                            <div className="sub">{elem.isActive ? elem.updatedAtString ? elem.updatedAtString : elem.createdAtString : <>
+                                <span className="activate badge badge-5" onClick={() => onActivate(elem)}>
+                                    <span>Prévisionnel</span>
+                                    <span className="tooltip">Activer</span>
+                                </span>
+                            </>}
+                            </div>
                         </div>
                         <div className="col-2">
                             <div className="name">{elem.name}</div>
