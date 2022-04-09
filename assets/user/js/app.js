6d3b19232124c5525c38ea55f4ef40ed802e006b
Starozytny <starozytny@hotmail.fr>
2022-04-09 15:08:02 +0200

    register

--- assets/user/js/app.js
+++ assets/user/js/app.js
@@ -8,7 +8,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Menu }  from "@dashboardComponents/Layout/Menu";
 import { Theme } from "@dashboardComponents/Theme";
-import { Notifications } from "@dashboardComponents/Notifications";
 
 Routing.setRoutingData(routes);
 
@@ -20,9 +19,4 @@ if(menu) {
 const theme = document.getElementById("theme");
 if(theme){
     render(<Theme />, theme)
-}
-
-const notifications = document.getElementById("notifications");
-if(notifications){
-    render(<Notifications {...notifications.dataset} />, notifications)
 }
